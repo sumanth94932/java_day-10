9e1936fc450c3d5275b93bca24e9f2578f2b4a59
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-11 16:21:54 +0530

    Add files via upload

---
+++ method.java
@@ -0,0 +1,24 @@
+class main{
+    static void a(){
+        System.out.println("HELLO");
+    }
+    static void a(int b){
+        System.out.println("HELLO" +b);
+    }
+    static void a(int b,int c)
+    {
+        System.out.println("HELLO " +b+" "+c);
+        }
+    static void a(int c,int b,int d)
+    {
+        System.out.println("HELLO" +b+" "+c+" "+d);
+    }
+    public static void main(String args[])
+    {
+    a();
+    a(6);
+    a(4,5);
+    }
+}
+
+    
