cbcca5e693b3af536cf1cb4e726b3cd340786cad
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-11 16:20:08 +0530

    Add files via upload

---
+++ accpri.java
@@ -0,0 +1,11 @@
+class accpri
+{
+    private static void s()
+    {
+        System.out.println("hello students");
+    }
+         public static void main(String args[])
+         {
+            s();
+         }
+}/*       hello students  */
