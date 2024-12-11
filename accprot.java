843d78f2c7322ec0236212146aa9c22a6f3a81fd
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-11 16:20:25 +0530

    Add files via upload

---
+++ accprot.java
@@ -0,0 +1,11 @@
+class accprot
+{
+    protected static void s()
+    {
+        System.out.println("hi mama");
+    }
+         public static void main(String args[])
+         {
+            s();
+         }
+}/*  hi mama    */
