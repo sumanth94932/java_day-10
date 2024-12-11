ec96d204044aaf6c23fa3094763621ce31e7dca7
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-11 16:22:11 +0530

    Add files via upload

---
+++ nonvoid.java
@@ -0,0 +1,12 @@
+class nonvoid
+{
+    public static String s()
+    {
+        return "HELLO";
+    }
+         public static void main(String args[])
+         {
+            String res=s();
+            System.out.println(res);
+         }
+}
