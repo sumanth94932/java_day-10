9edaefc792113a6ad96cef0fa1ba6ae013ab40e4
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-11 16:19:51 +0530

    Add files via upload

---
+++ accdefault.java
@@ -0,0 +1,11 @@
+class accdefault
+{
+    private static void s()
+    {
+        System.out.println("hello students");
+    }
+         public static void main(String args[])
+         {
+            s();
+         }
+}
